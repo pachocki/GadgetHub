418a6c9b2e996be81b5578e672825a3e52184f3b
Wojciech Pachocki <wojtek.kodehode@gmail.com>
2023-07-05 01:50:22 +0200

    updt

--- frontend/pages/api/checkout.js
+++ frontend/pages/api/checkout.js
@@ -2,20 +2,17 @@ import { mongooseConnect } from "@/lib/mongoseConnect";
 import { Product } from "@/models/Product";
 import { Order } from "@/models/Order";
 import cors from "cors";
-import stripePackage from "stripe";
-
-const stripe = stripePackage(process.env.STRIPE_SK);
+const stripe = require("stripe")(process.env.STRIPE_SK);
 
 const corsOptions = {
   origin: "https://gadgethub-nine.vercel.app",
-  methods: "POST",
-  allowedHeaders: "Content-Type",
-  optionsSuccessStatus: 200, 
+  methods: ["POST"],
+  allowedHeaders: ["Content-Type"],
 };
 
-const corsHandler = cors(corsOptions);
-
 export default async function handler(req, res) {
+  const corsHandler = cors(corsOptions);
+
   try {
     await new Promise((resolve, reject) => {
       corsHandler(req, res, (err) => {
@@ -27,7 +24,7 @@ export default async function handler(req, res) {
     });
 
     if (req.method !== "POST") {
-      res.json("should be a POST request");
+      res.status(400).json({ error: "Invalid request method. Should be a POST request." });
       return;
     }
 
@@ -37,22 +34,22 @@ export default async function handler(req, res) {
     // Connect to MongoDB using mongoose
     await mongooseConnect();
 
-    const productsIds = cartProducts;
+    const productIds = cartProducts;
+
     // Retrieve the unique product IDs from the cartProducts array
-    const uniqueIds = [...new Set(productsIds)];
+    const uniqueIds = [...new Set(productIds)];
+
     // Fetch the product information for the unique product IDs
     const productsInfos = await Product.find({ _id: { $in: uniqueIds } });
 
-    let line_items = [];
+    let lineItems = [];
     for (const productId of uniqueIds) {
-      const productInfo = productsInfos.find(
-        (p) => p._id.toString() === productId
-      );
-      const quantity =
-        productsIds.filter((id) => id === productId)?.length || 0;
+      const productInfo = productsInfos.find((p) => p._id.toString() === productId);
+      const quantity = productIds.filter((id) => id === productId)?.length || 0;
+
       // If the product quantity is greater than 0 and product information is available, create a line item
       if (quantity > 0 && productInfo) {
-        line_items.push({
+        lineItems.push({
           quantity,
           price_data: {
             currency: "USD",
@@ -62,9 +59,10 @@ export default async function handler(req, res) {
         });
       }
     }
+
     // Create an order document in the database
     const orderDoc = await Order.create({
-      line_items,
+      line_items: lineItems,
       name,
       email,
       city,
@@ -73,19 +71,19 @@ export default async function handler(req, res) {
       country,
       paid: false,
     });
+
     // Create a checkout session using Stripe
     const session = await stripe.checkout.sessions.create({
-      line_items,
+      line_items: lineItems,
       mode: "payment",
       customer_email: email,
       success_url: process.env.PUBLIC_URL + "/cart?success=1",
       cancel_url: process.env.PUBLIC_URL + "/cart?canceled=1",
       metadata: { orderId: orderDoc._id.toString(), test: "ok" },
     });
+
     // Return the URL of the checkout session to the client
-    res.json({
-      url: session.url,
-    });
+    res.json({ url: session.url });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "An error occurred" });
--- frontend/pages/api/webhook.js
+++ frontend/pages/api/webhook.js
@@ -1,54 +1,61 @@
-const stripe = require("stripe")(process.env.STRIPE_SK);
 import { buffer } from "micro";
 import { Order } from "@/models/Order";
 import { mongooseConnect } from "@/lib/mongoseConnect";
+import stripe from "stripe";
 
 // The endpoint secret for verifying webhook events
 const endpointSecret =
   "whsec_634d3142fd2755bd61adaef74ce0504bd2044848c8aac301ffdb56339a0ca78d";
 
+// Initialize Stripe with the secret key
+const stripeClient = stripe(process.env.STRIPE_SK);
+
 export default async function handler(req, res) {
   // Connect to MongoDB using mongoose
   await mongooseConnect();
+
   // Extract the signature from the request headers
   const sig = req.headers["stripe-signature"];
 
-  let event;
-
   try {
-    // Construct the event object from the request payload and signature
-    event = stripe.webhooks.constructEvent(
+    // Verify the event using the Stripe webhook signature and payload
+    const event = stripeClient.webhooks.constructEvent(
       await buffer(req),
       sig,
       endpointSecret
     );
-  } catch (err) {
-    // Return an error response if the event verification fails
-    res.status(400).send(`Webhook Error: ${err.message}`);
-    return;
-  }
 
-  // Handle the event based on its type
-  switch (event.type) {
-    case "checkout.session.completed":
-      // Retrieve relevant data from the event object
-      const data = event.data.object;
-      const orderId = data.metadata.orderId;
-      const paid = data.payment_status === "paid";
-      // Update the order status to paid if the payment was successful
-      if (orderId && paid) {
-        await Order.findByIdAndUpdate(orderId, {
-          paid: true,
-        });
-      }
-      break;
-    default:
-      console.log(`Unhandled event type ${event.type}`);
+    // Handle the event based on its type
+    switch (event.type) {
+      case "checkout.session.completed":
+        // Retrieve relevant data from the event object
+        const session = event.data.object;
+        const orderId = session.metadata.orderId;
+        const paid = session.payment_status === "paid";
+
+        // Update the order status to paid if the payment was successful
+        if (orderId && paid) {
+          await Order.findByIdAndUpdate(orderId, {
+            paid: true,
+          });
+        }
+        break;
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    // Return a success response
+    res.status(200).send("ok");
+  } catch (error) {
+    console.error("Webhook Error:", error);
+
+    // Return an error response
+    res.status(400).send(`Webhook Error: ${error.message}`);
   }
-  // Return a success response
-  res.status(200).send("ok");
 }
 
 export const config = {
-  api: { bodyParser: false },
+  api: {
+    bodyParser: false,
+  },
 };
